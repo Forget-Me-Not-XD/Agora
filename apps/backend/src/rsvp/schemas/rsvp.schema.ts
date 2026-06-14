3a9481b103054f150420e2f6985f18e0b5cf9fa6
koek1 <geandredupreez1@gmail.com>
2026-06-14 19:51:09 +0200

    WIP: RSVP Mongoose Schema DTO's en Module

--- apps/backend/src/rsvp/schemas/rsvp.schema.ts
+++ apps/backend/src/rsvp/schemas/rsvp.schema.ts
@@ -12,8 +12,31 @@ export type RsvpDocument = HydratedDocument<Rsvp>;
 
 @Schema({ timestamps: true, collection: 'rsvps' })
 export class Rsvp {
-    @Prop({ required: true, type: Types.ObjectId, ref: 'User', index: true })
+    @Prop ({ required: true, type: Types.ObjectId, ref: 'User', index: true })
     event!: Types.ObjectId;
 
-    // TBT
+    @Prop ({ required: true, type: Types.ObjectId, ref: 'User', index: true })
+    user!: Types.ObjectId;
+
+    @Prop ({ required: true, enum: RsvpStatus, default: RsvpStatus.HANGING })
+    status!: RsvpStatus;
+
+    @Prop ({ required: true, trim: true })
+    qrPayload!: string;
+
+    @Prop ({ default: false })
+    checkdeIn!: string;
+
+    @Prop ({ default: false })
+    checkedIn!: boolean;
+
+    @Prop ({ type: Date, default: null })
+    checkedInAt!: Date | null;
+
+    createdAt?: Date;
+    updatedAt?: Date;
 }
+
+export const RsvpSchema = SchemaFactory.createForClass(Rsvp);
+
+RsvpSchema.index({ event: 1, user: 1}, { unique: true });
